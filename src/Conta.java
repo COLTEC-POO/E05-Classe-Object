b2e5ab5ff2278686af060b85b205e8be7503da07
Eduardo Contaldo <82826467+EduardoContaldo@users.noreply.github.com>
2022-09-29 17:24:34 -0300

    Update Conta.java

--- src/Conta.java
+++ src/Conta.java
@@ -85,6 +85,20 @@ public class Conta {
         return contaStr;
     }
 
+    public boolean equals(Object obj){
+        if(obj instanceof Conta) {
+            Conta objConta = (Conta) obj;
+            if (this.numConta == objConta.numConta) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
+    }
+
     public Cliente getTitular() {
         return titular;
     }
