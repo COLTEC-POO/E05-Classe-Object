c0ff9ac5fb3a28974daf64591235f18b5d8f6834
DevDuque <davihduque8@gmail.com>
2023-09-18 20:01:52 -0300

    Comecando a utilizacao de equals

--- src/Main.java
+++ src/Main.java
@@ -7,28 +7,23 @@ public class Main {
 //        Conta[] contas = Conta.regContas();
 
         // Inicializando sem chamar regContas
-        Conta[] contas = new Conta[2];
+        Conta[] contas = new Conta[1];
 
         // Usando constructor sem regContas
-        contas[0] = new Conta (1, "senha1", 1500.0, "Dono1", 2000, new Cliente.PessoaFisica("Cliente1", "Endereco1", "CPF2", 25, 'F'));
-
-        contas[1] = new Conta (2, "senha2", 1500.0, "Dono2", 2000, new Cliente.PessoaFisica("Cliente2", "Endereco2", "CPF2", 25, 'F'));
+        contas[0] = new Conta (1, "senha1", 1500.0, "Dono1", 2000, new Cliente.PessoaFisica("Cliente1", "Endereco1", "CPF1", 25, 'F'));
 
         // Fazendo saques com cliente cadastrado no index 0
         contas[0].depositar(2000.0);
         contas[0].sacar(550.0);
 
-        // Fazendo saques com cliente cadastrado no index 0
-        contas[1].depositar(2000.0);
-        contas[1].sacar(550.0);
-
         // Imprime dados dos clientes cadastrados
         for (Conta conta : contas) {
-//            Cliente cliente = conta.getCliente();
+            Cliente cliente = conta.getCliente();
             System.out.println("=== Dados bancarios ===");
             System.out.println(conta.toString());
             System.out.println();
 
+            // Impressao de somente cliente
 //            System.out.println(cliente.toString());
 
             System.out.println("=== Extrato da Conta ===");
